dba4a3abad28a95f3a50dd507cd90791a53cb44b
Tabs <tabuaccristianjay@gmail.com>
2025-10-19 17:55:42 +0800

    Small fix on auth_Form.html

--- templatetags/form_extras.py
+++ templatetags/form_extras.py
@@ -1,6 +1,11 @@
+# dashboard/templatetags/form_extra.py
 from django import template
+
 register = template.Library()
 
-@register.filter
+@register.filter(name="add_class")
 def add_class(field, css):
-    return field.as_widget(attrs={"class": css})
+    """Append CSS classes to a bound field’s widget."""
+    attrs = field.field.widget.attrs.copy()
+    attrs["class"] = (attrs.get("class", "") + " " + css).strip()
+    return field.as_widget(attrs=attrs)
