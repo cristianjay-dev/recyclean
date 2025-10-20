a029f5ccda8c281e626033c999caf693c0774648
Tabs <tabuaccristianjay@gmail.com>
2025-10-20 13:14:05 +0800

    Fix issue templatesyntax error

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,5 +1,5 @@
 # dashboard/urls.py
-from django.urls import path
+from django.urls import path, reverse_lazy
 from django.http import HttpResponse
 from . import views
 from django.contrib.auth import views as auth_views
@@ -22,31 +22,30 @@ urlpatterns = [
     path(
         "password_reset/",
         auth_views.PasswordResetView.as_view(
-            template_name="password_reset_form.html",            # you provided this
-            # Optional (uncomment if you add them):
-            # email_template_name="password_reset_email.txt",
-            # subject_template_name="password_reset_subject.txt",
+            template_name="registration/password_reset_form.html",
+            success_url=reverse_lazy("password_reset_done"),
         ),
         name="password_reset",
     ),
     path(
         "password_reset/done/",
         auth_views.PasswordResetDoneView.as_view(
-            template_name="password_reset_done.html"
+            template_name="registration/password_reset_done.html",
         ),
         name="password_reset_done",
     ),
     path(
         "reset/<uidb64>/<token>/",
         auth_views.PasswordResetConfirmView.as_view(
-            template_name="password_reset_confirm.html"
+            template_name="registration/password_reset_confirm.html",
+            success_url=reverse_lazy("password_reset_complete"),
         ),
         name="password_reset_confirm",
     ),
     path(
         "reset/done/",
         auth_views.PasswordResetCompleteView.as_view(
-            template_name="password_reset_complete.html"
+            template_name="registration/password_reset_complete.html",
         ),
         name="password_reset_complete",
     ),
